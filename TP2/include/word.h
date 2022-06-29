5d38c0a234334f657e6db734d50a8416c5ef325f
Bárbara Martins <martins.barbara04@gmail.com>
2022-06-29 16:32:57 -0300

    Add static alphabet variable

--- TP2/include/word.h
+++ TP2/include/word.h
@@ -8,21 +8,21 @@ class Word {
 protected:
     std::string _word;
     int _count;
+    static Vector<char> _alphabet;
 
 public:
     Word();
+    explicit Word(std::string word);
     Word(std::string word, int count);
 
     std::string getPlayerName() const { return _word; }
     int getTotalAmount() const { return _count; }
+    Vector<char> getAlphabet() const { return _alphabet; }
     void setPlayerName(std::string word) { _word = std::move(word); }
     void setTotalAmount(int count) { _count = count; }
+    static void setAlphabet(Vector<char> alphabet) { _alphabet = alphabet; }
 
     void print();
-
-    bool operator< (const Word &other) {
-//        return lhs[0] < rhs[0];
-    }
 };
 
 #endif //WORD_H
